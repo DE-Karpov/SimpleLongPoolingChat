0eda4d5520ebcaa635b187119e9dcc937892203a
DE-Karpov <mecazor.lucky@gmail.com>
2019-10-19 02:09:15 +0300

    refactoring

--- src/main/java/lab/chat/demo/controllers/MessagesController.java
+++ src/main/java/lab/chat/demo/controllers/MessagesController.java
@@ -10,9 +10,6 @@ import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/lab/chat/demo/security/config/SecurityConfig.java
+++ src/main/java/lab/chat/demo/security/config/SecurityConfig.java
@@ -12,8 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-import javax.servlet.Filter;
-
 @Configuration
 @ComponentScan("lab.chat")
 @EnableGlobalMethodSecurity(prePostEnabled = true)
